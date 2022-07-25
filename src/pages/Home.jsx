71ecc678e235ce038f06e8a3d686e733ffab849f
boom <nuntipatcpe@gmail.com>
2022-07-25 19:45:52 +0700

    refactor home add link btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ import img from '../assets/img/bg-2.png'
 
 import CardComponent from "../component/CardComponent";
 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+
 function Home() {
   return (
     <div className="home">
@@ -11,7 +14,8 @@ function Home() {
         <div className="background">
           <div className="img">  
             <h1>Grow Shop</h1>
-            <button>Products</button>
+            <Link to="/products" className="btn-product"> Products</Link>
+            {/* <button>Products</button> */}
           </div>
         </div>
         <div className="top">
