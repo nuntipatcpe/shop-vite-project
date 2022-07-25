6d56a75c89a48090cbf59493bd668ee6a1b3552b
boom <nuntipatcpe@gmail.com>
2022-07-25 16:04:56 +0700

    Home page success

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function navbar() {
+  const [productHendle,setProductHendle] = useState(false);
+
   return (
     <div className="nav-bar">
+      <div className="container">
         <div className="logo">
-            <ul className="menu">
-                <h1>SHOP</h1>
-                <li className="item"><a href="">Product</a></li>
-                <li className="item"><a href="">Blog</a></li>
-                <div className="menu-login">
-                    <li className="item"><a href="">Log</a></li>
-                    <p>/</p>
-                    <li className="item"><a href="">Sing-up</a></li>
-                </div>
-                <div className="cart">cart 0</div>
-            </ul>
+          <h1>SHOP</h1>
         </div>
+        <ul className="menu">
+          <li className='item' onClick={()=>setProductHendle(!productHendle)}>
+            <h1 className="po">Product</h1>
+              <div className={productHendle ? 'bar active' : 'bar' }>
+                <p><a href="#seed">Seed</a></p>
+                <p><a href="#tools">Planting tools</a></p>
+              </div>
+          </li>
+          <li className="item">
+            <a href="">Blog</a>
+          </li>
+          <div className="menu-login">
+            <li>
+              <a href="">Log</a>
+            </li>
+            <p>/</p>
+            <li className="item">
+              <a href="">Sing-up</a>
+            </li>
+          </div>
+          <div className="cart">cart 0</div>
+        </ul>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default navbar
+export default navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+
+import img from '../assets/img/bg-2.png'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="container">
+        <div className="background">
+          <div className="img">  
+            <h1>Grow Shop</h1>
+            <button>Products</button>
+          </div>
+        </div>
+        <div className="top">
+
+          
+          <h1>Top3</h1>
+          <div className="card-con">
+          
+            <h1>1st</h1>
+            <div className="card">
+              <img src={img} alt="img" />
+              <div className="topic">Cannabis sativa</div>
+              <div className="pice">$500</div>
+              <div className="btn">
+                <button className="add-cart">Add to cart</button>
+              <button className="buy">Buy</button>
+              </div>
+            </div>
+        
+            <h1>2st</h1>
+            <div className="card">
+              <img src={img} alt="img" />
+              <div className="topic">Cannabis sativa</div>
+              <div className="pice">$500</div>
+              <div className="btn">
+                <button className="add-cart">Add to cart</button>
+              <button className="buy">Buy</button>
+              </div>
+            </div>
+        
+            <h1>3st</h1>
+            <div className="card">
+              <img src={img} alt="img" />
+              <div className="topic">Cannabis sativa</div>
+              <div className="pice">$500</div>
+              <div className="btn">
+                <button className="add-cart">Add to cart</button>
+              <button className="buy">Buy</button>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
